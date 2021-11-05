2e920e2dc056378987394a256c6402dcdd9db392
carolinecole25 <carolinecole25@gmail.com>
2021-11-05 17:46:29 +0000

    inspiration section added

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('lighting_inspiration/', views.lighting_inspiration, name='lighting_inspiration'),
+    path('dining_inspiration/', views.dining_inspiration, name='dining_inspiration'),
+    path('home_decor_inspiration/', views.home_decor_inspiration, name='home_decor_inspiration'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,21 @@ def index(request):
     """ A view to return the index page. """
 
     return render(request, 'home/index.html')
+
+
+def lighting_inspiration(request):
+    """ A view to return the lighting inspiration page. """
+
+    return render(request, 'home/lighting_inspiration.html')
+
+
+def dining_inspiration(request):
+    """ A view to return the dining inspiration page. """
+
+    return render(request, 'home/dining_inspiration.html')
+
+
+def home_decor_inspiration(request):
+    """ A view to return the home decor inspiration page. """
+
+    return render(request, 'home/home_decor_inspiration.html')
