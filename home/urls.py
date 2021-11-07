d45250be5ffaea9925a54cee46eaca20b5d28eeb
carolinecole25 <carolinecole25@gmail.com>
2021-11-07 20:07:09 +0000

    added contact page, finished our story page

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('lighting_inspiration/', views.lighting_inspiration, name='lighting_inspiration'),
     path('dining_inspiration/', views.dining_inspiration, name='dining_inspiration'),
     path('home_decor_inspiration/', views.home_decor_inspiration, name='home_decor_inspiration'),
+    path('our_story/', views.our_story, name='our_story'),
+    path('contact_us/', views.contact_us, name='contact_us'),
 ]
--- home/views.py
+++ home/views.py
@@ -24,3 +24,13 @@ def home_decor_inspiration(request):
     """ A view to return the home decor inspiration page. """
 
     return render(request, 'home/home_decor_inspiration.html')
+
+def our_story(request):
+    """ A view to return the our story page. """
+
+    return render(request, 'home/our_story.html')
+
+def contact_us(request):
+    """ A view to return the contact us page. """
+
+    return render(request, 'home/contact_us.html')
